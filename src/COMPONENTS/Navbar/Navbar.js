972cec9355fb7189fcfd39e560dd851b8d9082b3
MICyber <126847244+MICyber@users.noreply.github.com>
2024-02-23 18:04:58 +0530

    Language_DropdownIcon

--- src/COMPONENTS/Navbar/Navbar.js
+++ src/COMPONENTS/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import logo from "../../ASSETS/logo.png";
 import Dropdown from "react-bootstrap/Dropdown";
 import { Link } from "react-router-dom";
+import { GrLanguage } from "react-icons/gr";
 const Navbar = () => {
   const [shows3, setshows3] = useState(false);
   return (
@@ -48,23 +49,10 @@ const Navbar = () => {
         </div>
         <div className="right">
           <div className="s2">
-            <Dropdown>
-              <Dropdown.Toggle variant="" id="language-dropdown">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                  />
-                </svg>
-              </Dropdown.Toggle>
+          <Dropdown>
+  <Dropdown.Toggle variant="" id="language-dropdown">
+    <GrLanguage className="w-6 h-6" />
+  </Dropdown.Toggle>
 
               <Dropdown.Menu>
                 <Dropdown.Item>සිංහල</Dropdown.Item>
