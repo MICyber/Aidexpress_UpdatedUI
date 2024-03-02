53b02c78b8e7bc03da5302642f7e9598a5b6414a
MICyber <126847244+MICyber@users.noreply.github.com>
2024-03-02 18:28:20 +0530

    Translated banners

--- src/COMPONENTS/Category/HomeCategories.js
+++ src/COMPONENTS/Category/HomeCategories.js
@@ -5,36 +5,38 @@ import img2 from "../../ASSETS/Images/track.png";
 import img3 from "../../ASSETS/Images/image_language.png";
 import img4 from "../../ASSETS/Images/image_donate.png";
 import img5 from "../../ASSETS/Images/image_sms.png"
+import { useTranslation } from 'react-i18next';
 
 const HomeCategories = () => {
+  const { t } = useTranslation();
   return (
     <div className="homecategories">
       <div className="container">
         <img src={img5} alt="img1" />
         <div className="content">
-          <h1>SMS Services</h1>
-          <p>Connect anytime, anywhere, no smartphone needed!</p>
+          <h1>{t("SMS")}</h1>
+          <p>{t("SMSDetails")}</p>
         </div>
       </div>
       <div className="container">
         <img src={img2} alt="img2" />
         <div className="content">
-          <h1>Track Your Donations</h1>
-          <p>Receive, track, collect at a bank or Samurdhi officer. Easy & secure.</p>
+          <h1>{t("Track")}</h1>
+          <p>{t("TrackDetails")}</p>
         </div>
       </div>
       <div className="container">
         <img src={img3} alt="img3" />
         <div className="content">
-          <h1>Multilingual Support</h1> 
-          <p>Sinhala, Tamil, and English.</p>
+          <h1>{t("Multilingual")}</h1> 
+          <p>{t("MultilingualDetails")}</p>
         </div>
       </div>
       <div className="container">
         <img src={img4} alt="img4" />
         <div className="content">
-          <h1>Support the Cause</h1>
-          <p>Donate to help those in need</p>
+          <h1>{t("Support")}</h1>
+          <p>{t("SupportDetails")}</p>
         </div>
       </div>
     </div>
