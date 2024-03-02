61df5d3bc8abb7afa355727684a018765f67d896
Samadhi Lochana Ediriwickrama <146553536+Ediriwickrama1@users.noreply.github.com>
2024-03-02 20:04:24 +0530

    translate

--- src/COMPONENTS/Category/HomeCategories.js
+++ src/COMPONENTS/Category/HomeCategories.js
@@ -14,49 +14,29 @@ const HomeCategories = () => {
       <div className="container">
         <img src={img5} alt="img1" />
         <div className="content">
-<<<<<<< HEAD
           <h1>{t("category1")}</h1>
           <p>{t("category1title2")}</p>
-=======
-          <h1>{t("SMS")}</h1>
-          <p>{t("SMSDetails")}</p>
->>>>>>> f07516067f922c9fe8f5916a4f65e7207e5098af
         </div>
       </div>
       <div className="container">
         <img src={img2} alt="img2" />
         <div className="content">
-<<<<<<< HEAD
           <h1>{t("category2")}</h1>
           <p>{t("category2title2")}</p>
-=======
-          <h1>{t("Track")}</h1>
-          <p>{t("TrackDetails")}</p>
->>>>>>> f07516067f922c9fe8f5916a4f65e7207e5098af
         </div>
       </div>
       <div className="container">
         <img src={img3} alt="img3" />
         <div className="content">
-<<<<<<< HEAD
           <h1>{t("category3")}</h1> 
           <p>{t("category3title2")}</p>
-=======
-          <h1>{t("Multilingual")}</h1> 
-          <p>{t("MultilingualDetails")}</p>
->>>>>>> f07516067f922c9fe8f5916a4f65e7207e5098af
         </div>
       </div>
       <div className="container">
         <img src={img4} alt="img4" />
         <div className="content">
-<<<<<<< HEAD
           <h1>{t("category4")}</h1>
           <p>{t("category4title2")}</p>
-=======
-          <h1>{t("Support")}</h1>
-          <p>{t("SupportDetails")}</p>
->>>>>>> f07516067f922c9fe8f5916a4f65e7207e5098af
         </div>
       </div>
     </div>
