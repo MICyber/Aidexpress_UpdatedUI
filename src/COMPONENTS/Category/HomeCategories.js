b8db34718deb090df41a2a059c36eee9218ca8fd
Samadhi Lochana Ediriwickrama <146553536+Ediriwickrama1@users.noreply.github.com>
2024-03-02 19:56:29 +0530

    translate

--- src/COMPONENTS/Category/HomeCategories.js
+++ src/COMPONENTS/Category/HomeCategories.js
@@ -5,36 +5,38 @@ import img2 from "../../ASSETS/Images/track.png";
 import img3 from "../../ASSETS/Images/image_language.png";
 import img4 from "../../ASSETS/Images/image_donate.png";
 import img5 from "../../ASSETS/Images/image_sms.png"
+import { useTranslation } from 'react-i18next';
 
 const HomeCategories = () => {
+  const { t } = useTranslation();
   return (
     <div className="homecategories">
       <div className="container">
         <img src={img5} alt="img1" />
         <div className="content">
-          <h1>SMS Services</h1>
-          <p>Connect anytime, anywhere, no smartphone needed!</p>
+          <h1>{t("category1")}</h1>
+          <p>{t("category1title2")}</p>
         </div>
       </div>
       <div className="container">
         <img src={img2} alt="img2" />
         <div className="content">
-          <h1>Track Your Donations</h1>
-          <p>Receive, track, collect at a bank or Samurdhi officer. Easy & secure.</p>
+          <h1>{t("category2")}</h1>
+          <p>{t("category2title2")}</p>
         </div>
       </div>
       <div className="container">
         <img src={img3} alt="img3" />
         <div className="content">
-          <h1>Multilingual Support</h1> 
-          <p>Sinhala, Tamil, and English.</p>
+          <h1>{t("category3")}</h1> 
+          <p>{t("category3title2")}</p>
         </div>
       </div>
       <div className="container">
         <img src={img4} alt="img4" />
         <div className="content">
-          <h1>Support the Cause</h1>
-          <p>Donate to help those in need</p>
+          <h1>{t("category4")}</h1>
+          <p>{t("category4title2")}</p>
         </div>
       </div>
     </div>
