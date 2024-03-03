4b77e9590323a0df53c4b9d9280f3be668100bf5
Samadhi Lochana Ediriwickrama <146553536+Ediriwickrama1@users.noreply.github.com>
2024-03-03 14:33:27 +0530

    donate

--- src/COMPONENTS/Donation/donateform.js
+++ src/COMPONENTS/Donation/donateform.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Form, Col, Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Navbar from '../Navbar/Navbar'
-import "./donateform.css";
+import './donateform.css';
+
 
 const donateform = () => {
   return (
