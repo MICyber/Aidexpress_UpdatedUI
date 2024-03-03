e1628932bcbb3d1905c61f8e8cd90c707654ee48
MICyber <126847244+MICyber@users.noreply.github.com>
2024-03-03 20:36:01 +0530

    Authuntication part for the user (Connected with firebase)

--- src/PAGES/Auth/Login.js
+++ src/PAGES/Auth/Login.js
@@ -1,50 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Navbar from '../../COMPONENTS/Navbar/Navbar'
-import './AuthPage.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../../COMPONENTS/Navbar/Navbar';
+import './AuthPage.css';
+import firebase from './firebaseConfig';
+
 const Login = () => {
-    return (
-        <div className='authpage'>
-            <Navbar reloadnavbar={false}/>
+  const [email, setEmail] = useState('');
+  const [pass, setPassword] = useState('');
 
-            <div className='authcont'>
-                <img src='https://i.pinimg.com/564x/80/37/8c/80378c968a1dbd5eefaff4c102a89205.jpg'
-                    alt='login' />
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await firebase.auth().signInWithEmailAndPassword(email, pass);
+      if (user) {
+        alert("Login Successfully");
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
-                <form className='authform'>
-                    <h1>Login</h1>
-                    <div className='formgroup'>
-                        <label htmlFor='email'>Email</label>
-                        <input type='email' id='email' />
-                    </div>
+  return (
+    <div className='authpage'>
+      <Navbar reloadnavbar={false} />
+      <div className='authcont'>
+        <img src='https://i.pinimg.com/564x/80/37/8c/80378c968a1dbd5eefaff4c102a89205.jpg' alt='login' />
+        <form className='authform'>
+          <h1>Login</h1>
+          <div className='formgroup'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className='formgroup'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='pass' onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <Link to='/forgotpassword' className='stylenone'>
+            <p>Forgot password?</p>
+          </Link>
+          <button onClick={submit}>Login</button>
+          <h2 className='or'>OR</h2>
+          <Link to='/SignUpUser' className='stylenone'>
+            <button className='btn'>Signup</button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-                    <div className='formgroup'>
-                        <label htmlFor='password'>Password</label>
-                        <input type='password' id='password' />
-                    </div>
-
-                    <Link to='/forgotpassword'
-                        className='stylenone'
-                    >
-                        <p>Forgot password?</p>
-                    </Link>
-
-                    <Link to='/UserProfile/accountsettings'
-                        className='stylenone'
-                    >
-                        <button className='btn'>Login</button>
-                    </Link>
-
-                    <h2 className='or'>OR</h2>
-                    <Link to='/SignUpUser'
-                        className='stylenone'
-                    >
-                        <button className='btn'>Signup</button>
-                    </Link>
-                </form>
-            </div>
-        </div>
-    )
-}
-
-export default Login
+export default Login;
