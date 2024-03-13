d21ddb0d9458c773ce34452e3c645daa00da84de
Samadhi Lochana Ediriwickrama <146553536+Ediriwickrama1@users.noreply.github.com>
2024-03-13 18:11:32 +0530

    Agreement page created

--- src/COMPONENTS/Donation/DonationForm.js
+++ src/COMPONENTS/Donation/DonationForm.js
@@ -103,7 +103,7 @@ const DonationForm = () => {
 export default DonationForm;*/
 
 import React, { useState } from "react";
-import { Form, Col, Button } from 'react-bootstrap';
+import { Form, Col, Button,Modal } from 'react-bootstrap';
 import Navbar from "../Navbar/Navbar";
 import './DonationForm.css';
 
@@ -123,7 +123,7 @@ const DonationForm = () => {
     branch: "",
     accountName: "",
   });
-
+  const [showModal, setShowModal] = useState(false);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -144,6 +144,10 @@ const DonationForm = () => {
     e.preventDefault();
     // Handle form submission logic here
     console.log("Form submitted:", formData);
+  };
+
+  const handleModal = () => {
+    setShowModal(!showModal); // Toggle modal visibility
   };
 
   return (
@@ -203,7 +207,7 @@ const DonationForm = () => {
           <Form.Label>Amount</Form.Label>
           <Form.Control
             type="text"
-            placeholder="$ USD"
+            placeholder="Rs"
             name="amount"
             value={formData.amount}
             onChange={handleChange}
@@ -322,6 +326,13 @@ const DonationForm = () => {
                   required
                 />
               </Form.Group>
+              <Form.Group id="formGridCheckbox">
+                <Button variant= "outline-primary" onClick={handleModal}>View Agreement</Button>
+                <Form.Check type="checkbox" label="I read the Disclosure and Agreed to the Terms"/>
+
+              </Form.Group>
+
+            
             </>
           )}
 
@@ -329,6 +340,30 @@ const DonationForm = () => {
             Submit
           </Button>
         </Form>
+
+        <Modal show={showModal} onHide={handleModal} >
+        <Modal.Header closeButton>
+        <Modal.Title>Donation Agreement</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ fontSize: "9px" }}>
+          <p>
+          This Gift/ Donation Agreement (“Agreement”) is made this ____ day of _______ between [Donor’s Names] and their assignee (hereinafter referred to as “the Donor”), and [Your Organization] an [arts nonprofit organization located in Indianapolis, Indiana]. The Donor and [Your Organization] agree as follows:
+          </p>
+          <p>
+          Donor Commitment. The Donor hereby pledges to [Your Organization] the sum of [insert amount] or more, which as provided for herein is designated for the benefit of [Fund Name] Endowment.
+                            Donor Purpose.
+                            Purpose. It is understood and agreed that the gift will be used for the following purpose or purposes: To establish an endowment from which the annual interest earnings will be used to [state purpose].
+                            Payment. It is further understood and agreed that the gift will be paid in full on or before [insert date].
+          </p>
+          <p>
+          It is also understood and agreed that the gift funds as received may be invested by a third-party that shall best determine investment options for this endowment fund (see item #4 below). The spending policy for the endowment will be the policy set forth and approved by [Your Organization] Board of Directors which will likely include the use of annual interest earnings only and not invade the principal of the fund to protect and perpetuate growth.
+          </p>
+        </Modal.Body>
+        </Modal>
+
+      
+
+        
       </div>
     </div>
   );
