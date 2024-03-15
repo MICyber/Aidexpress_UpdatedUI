add8f3fa8e6061594a71123df06f20d4868a99da
Thanaal <thanaal.20220062@iit.ac.lk>
2024-03-15 20:15:45 +0530

    Handle Error

--- src/PAGES/Auth/Signup.js
+++ src/PAGES/Auth/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../COMPONENTS/Navbar/Navbar';
 import firebase from './firebaseConfig';
+import { useTranslation } from 'react-i18next';
 
 
 const Signup = () => {
@@ -10,6 +11,8 @@ const Signup = () => {
   const [email, setEmail] = useState('');
   const [pass, setPassword] = useState('');
   const [confpass, setConfirmPassword] = useState('');
+  const { t } = useTranslation();
+
 
   const submit = async (e) => {
     e.preventDefault();
