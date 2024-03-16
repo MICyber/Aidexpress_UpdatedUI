9382a934005fde2a06263f82ec621b13d70f6dd8
MICyber <126847244+MICyber@users.noreply.github.com>
2024-03-17 00:45:11 +0530

    Creating Officer page table for the application

--- src/COMPONENTS/Order/OrderSuccessful.js
+++ src/COMPONENTS/Order/OrderSuccessful.js
@@ -1,157 +1,81 @@
-import React, { useState } from 'react'
-import './OrderSuccessful.css'
+import React, { useState } from 'react';
+import './OrderSuccessful.css';
 
 const OrderSuccessful = ({ orderid, message, redirecto }) => {
-
     const [orderdata, setorderdata] = useState({
-      
-    })
+        nameOfHouseholder: "",
+        address: "",
+        nicNumber: "",
+        telephoneNumber: "",
+        age: "",
+        familyMembers: "",
+        maritalStatus: "",
+        singleParent: "",
+        divisionalSecretariat: "",
+        gramaSevakaDivision: ""
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setorderdata(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
     return (
-        <div
-            className='OrdersSuccessful'
-        >
-            <button className='popup__close-btn'
-                onClick={() => {
-
-                    if(redirecto == 'userorders'){
-                        window.location.href = '/user/yourorders'
-                    }
-                    
-                }}
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-
+        <div className='OrdersSuccessful'>
             <div className='confirmationcont'>
-                <div className='c1'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
-                    </svg>
-                    <h2>{message}</h2>
-                </div>
-
-                <div className='c2'>
-                    <h2>Order Summary</h2>
-                    <div>
-                        <p>Order Number</p>
-                        <p>{orderdata?.OrderNo}</p>
-                    </div>
-                    <div>
-                        <p>Order Date</p>
-                        <p>{
-                            orderdata.OrderDate
-                        }</p>
-                    </div>
-
-                    <div>
-                        <p>Name</p>
-                        <p>{
-                            orderdata.CustomerName
-                        }</p>
-                    </div>
-
-                    <div>
-                        <p>Email</p>
-                        <p>
-                            {
-                                orderdata.CustomerEmail
-                            }
-                        </p>
-                    </div>
-
-                    <div>
-                        <p>Order Subtotal</p>
-                        <p>$ {orderdata.SubTotal}</p>
-                    </div>
-
-                    <div>
-                        <p>Payment Method</p>
-                        <p>{orderdata.PaymentType}</p>
-                    </div>
-
-                    <div>
-                        <p>Shipping Address</p>
-                        <p>{orderdata.CustomerShipToAddress
-                        }</p>
-                    </div>
-
-                    <div>
-                        <p>Shipping Charges</p>
-                        <p>$ {orderdata.ShippingCharges}</p>
-                    </div>
-
-                    <div>
-                        <p>Tax</p>
-                        <p>$ {orderdata.Tax}</p>
-                    </div>
-
-                    <div>
-                        <p>Total</p>
-                        <p>$ {orderdata.Total}</p>
-                    </div>
-
-                </div>
-
                 <div className='c3'>
+                    <center><h2>Personal Details</h2></center>
                     <table>
-                        <thead>
-                            <tr>
-                                <th>Sno.</th>
-                                <th>Product</th>
-                                <th>Price</th>
-                                <th>Quantity</th>
-                                <th>Total Price</th>
-                            </tr>
-                        </thead>
-
                         <tbody>
-                            {
-                                orderdata?.OrderItems && orderdata.OrderItems.map((item, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <td>
-                                                <p>{index + 1}</p>
-                                            </td>
-                                            <td><p>{item.ProductName}</p></td>
-                                            <td><p>${item.Price}</p></td>
-                                            <td><p>{item.Quantity}</p></td>
-                                            <td><p>${item.Price * item.Quantity}</p></td>
-                                        </tr>
-                                    )
-                                })
-                            }
+                            <tr>
+                                <td>Name of householder:</td>
+                                <td><input type="text" name="nameOfHouseholder" value={orderdata.nameOfHouseholder} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>Address:</td>
+                                <td><input type="text" name="address" value={orderdata.address} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>NIC Number:</td>
+                                <td><input type="text" name="nicNumber" value={orderdata.nicNumber} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>Telephone Number:</td>
+                                <td><input type="text" name="telephoneNumber" value={orderdata.telephoneNumber} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>Age:</td>
+                                <td><input type="text" name="age" value={orderdata.age} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>Names of Family Members:</td>
+                                <td><input type="text" name="familyMembers" value={orderdata.familyMembers} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>Marital Status:</td>
+                                <td><input type="text" name="maritalStatus" value={orderdata.maritalStatus} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>Single Parent:</td>
+                                <td><input type="text" name="singleParent" value={orderdata.singleParent} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>Divisional Secretariat:</td>
+                                <td><input type="text" name="divisionalSecretariat" value={orderdata.divisionalSecretariat} onChange={handleChange} /></td>
+                            </tr>
+                            <tr>
+                                <td>Grama Sevaka Division:</td>
+                                <td><input type="text" name="gramaSevakaDivision" value={orderdata.gramaSevakaDivision} onChange={handleChange} /></td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
-
-                <div className='totalcont'>
-                    <div>
-                        <p>Subtotal</p>
-                        <p>$ {orderdata.SubTotal}</p>
-                    </div>
-
-                    <div>
-                        <p>Shipping</p>
-                        <p>$ {orderdata.ShippingCharges}</p>
-                    </div>
-
-                    <div>
-                        <p>Tax</p>
-                        <p>$ {orderdata.Tax}</p>
-                    </div>
-
-                    <div>
-                        <p>Total</p>
-                        <p>$ {orderdata.Total}</p>
-                    </div>
-                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default OrderSuccessful
-
-/*fsfd */
+export default OrderSuccessful;
