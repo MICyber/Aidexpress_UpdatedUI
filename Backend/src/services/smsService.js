517706b8a0f06a63f9816253cdda0beaaa9c1ee2
MICyber <126847244+MICyber@users.noreply.github.com>
2024-03-23 14:53:01 +0530

    fixed sms

--- Backend/src/services/smsService.js
+++ Backend/src/services/smsService.js
@@ -3,11 +3,12 @@ const twilio = require('twilio');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Set your environment variable
 const authToken = process.env.TWILIO_AUTH_TOKEN; // Set your environment variable
-const client = new twilio("ACdd8505b40d9163fc0e5d933ef197d406", "ce7317e5e5421268a58bba08c2c9e453");
+const client = new twilio("AC5e6d3b7896dbabf3a1f613a2276248b1", "2e9f0121d0dcc53aea65f9fea09642ca");
 const fromNumber = process.env.TWILIO_PHONE_NUMBER; // Your Twilio phone number
-const messagingServiceSId ="MG19e475956088e87388ee487adff89df7";
+const messagingServiceSId ="MGd3cee75bdab1955ef3f3fea516cb1949";
 async function sendSMS(to, message) {
   try {
+    console.log(`Sending message to: ${to}, Message: ${message}`);
     const messageResponse = await client.messages.create({
       body: message,
       to: to, 
