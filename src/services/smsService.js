2d20a4352ef908328a370b381b9d53c6ebcd0682
MICyber <126847244+MICyber@users.noreply.github.com>
2024-03-27 18:24:21 +0530

    auuth token change

--- src/services/smsService.js
+++ src/services/smsService.js
@@ -2,7 +2,7 @@
 const twilio = require('twilio');
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Set your environment variable
 const authToken = process.env.TWILIO_AUTH_TOKEN; // Set your environment variable
-const client = new twilio("AC5e6d3b7896dbabf3a1f613a2276248b1", "338e1a6533ab4ef2df77576a9525684b");
+const client = new twilio("AC5e6d3b7896dbabf3a1f613a2276248b1", "42edbfdde679be491e00b6e7457fe453");
 const fromNumber = process.env.TWILIO_PHONE_NUMBER; // Your Twilio phone number
 
 async function sendSMS(to, messageText) {
